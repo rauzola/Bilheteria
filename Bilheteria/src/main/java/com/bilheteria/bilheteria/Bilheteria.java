65b54fb64604163220c1c5f7075e4a02a6063b8a
Raul Sigoli <raul_sigoli@hotmail.com>
2024-05-02 22:04:11 -0300

    inicial :100:

---
+++ Bilheteria/src/main/java/com/bilheteria/bilheteria/Bilheteria.java
@@ -0,0 +1,18 @@
+
+package com.bilheteria.bilheteria;
+
+/**
+ *
+ * @author Raul Sigoli
+ */
+
+public class Bilheteria {
+    
+    public static void main(String[] args) {
+        
+        Home bilheteria = new Home();
+        bilheteria.setVisible(true);
+        System.out.println("Hello World!");
+        
+    }
+}
