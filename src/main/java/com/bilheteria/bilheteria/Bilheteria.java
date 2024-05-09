cf3ac80af28248651bbc6128c6989b54fbe8fe87
Raul Sigoli <raul_sigoli@hotmail.com>
2024-05-09 15:34:10 -0300

    feat: pegando os dados da api do IBGE

---
+++ src/main/java/com/bilheteria/bilheteria/Bilheteria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bilheteria.bilheteria;
+
+/**
+ *
+ * @author raul_
+ */
+
+public class Bilheteria {
+    public static void main(String[] args) {
+        // Criar uma instância da classe Estado
+        Estado estado = new Estado();
+
+        // Chamar o método responsável por obter os estados
+        estado.obterEstados();
+    }
+}
